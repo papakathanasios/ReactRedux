650c470ca1b742ee752079d59f81f53bb921d65f
Thanos Papakostas <papakostas.athanasios@nbg.gr>
2018-05-11 11:37:46 +0300

    end of module 5

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { PopTypes } from "react";
+import Header from "./common/Header";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from "react";
+//import {connect} from 'react-redux';
+//import * as courseActions from '../../actions/courseActions';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export  default CoursesPage;
